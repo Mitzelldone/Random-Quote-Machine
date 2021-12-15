34b8161c3acd42235d2836af3e09c6445a325249
DoneCode96 <mitzelldone.mvn@gmail.com>
2021-12-15 15:14:53 +0800

    Initial commits :octocat:

---
+++ main.js
@@ -0,0 +1,27 @@
+// The JS  will be loaded each time we load window
+window.onload = init;
+
+function init() {
+    generateQuote()
+}
+
+// This will run when we click the button
+function generateQuote() {
+    let quoteSize = quoteBank.length;
+    let randomIndex = Math.floor(Math.random() *quoteSize)
+    let randomQuoteData = quoteBank[randomIndex]
+
+    let twitterLink ="https://twitter.com/intent/tweet?hashtags=quotes,freecodecamp&related=freecodecamp&text="
+    
+    // Add The Quote
+    let quoteInApiFormat = randomQuoteData.quote.replace(/ /g, "%20")
+    twitterLink += '"' + quoteInApiFormat + '"'; 
+    
+    // Add The Author
+    let authorInApiFormat = randomQuoteData.author.replace(/ /g, "%20")
+    twitterLink += "%0D%0A - " + authorInApiFormat + "%0D%0A"; 
+
+    document.getElementById("tweet-quote").href = twitterLink;
+    document.getElementById("text").innerText = randomQuoteData.quote;
+    document.getElementById("author").innerText = randomQuoteData.author
+}
